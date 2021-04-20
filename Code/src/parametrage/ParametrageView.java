5515a9aa1d48e1e7b0e7e054d20532b28d3263f5
GGEphec <he201732@students.ephec.be>
2021-04-20 10:07:12 +0200

    Hardcodage des valeurs des produits

--- Code/src/parametrage/ParametrageView.java
+++ Code/src/parametrage/ParametrageView.java
@@ -167,119 +167,154 @@ public class ParametrageView {
 				Boutons.setBorder(transparent9px);
 				Boutons.setLayout(new GridLayout(6, 6, 0, 0));
 				//Première ligne
-					JButton btn00 = new JButton("New button");
-					Boutons.add(btn00);
-					
-					JButton btn01 = new JButton("New button");
-					Boutons.add(btn01);
-					
-					JButton btn02 = new JButton("New button");
-					Boutons.add(btn02);
-					
-					JButton btn03 = new JButton("New button");
-					Boutons.add(btn03);
-					
-					JButton btn04 = new JButton("New button");
-					Boutons.add(btn04);
-					
-					JButton btn05 = new JButton("New button");
-					Boutons.add(btn05);
+				JButton btn00 = new JButton("<html><center>Ticket rouge <br> 1,50€ </center></html>");
+				btn00.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn00);
 				
-				//Deuxième ligne
-					JButton btn10 = new JButton("New button");
-					Boutons.add(btn10);
-					
-					JButton btn11 = new JButton("New button");
-					Boutons.add(btn11);
-					
-					JButton btn12 = new JButton("New button");
-					Boutons.add(btn12);
-					
-					JButton btn13 = new JButton("New button");
-					Boutons.add(btn13);
-					
-					JButton btn14 = new JButton("New button");
-					Boutons.add(btn14);
-					
-					JButton btn15 = new JButton("New button");
-					Boutons.add(btn15);
-					
-				//Troisième ligne
-					JButton btn20 = new JButton("New button");
-					Boutons.add(btn20);
-					
-					JButton btn21 = new JButton("New button");
-					Boutons.add(btn21);
-					
-					JButton btn22 = new JButton("New button");
-					Boutons.add(btn22);
-					
-					JButton btn23 = new JButton("New button");
-					Boutons.add(btn23);
-					
-					JButton btn24 = new JButton("New button");
-					Boutons.add(btn24);
-					
-					JButton btn25 = new JButton("New button");
-					Boutons.add(btn25);
-					
-				//Quatrième ligne
-					JButton btn30 = new JButton("New button");
-					Boutons.add(btn30);
-					
-					JButton btn31 = new JButton("New button");
-					Boutons.add(btn31);
-					
-					JButton btn32 = new JButton("New button");
-					Boutons.add(btn32);
-					
-					JButton btn33 = new JButton("New button");
-					Boutons.add(btn33);
-					
-					JButton btn34 = new JButton("New button");
-					Boutons.add(btn34);
-					
-					JButton btn35 = new JButton("New button");
-					Boutons.add(btn35);
-					
-				//Cinquième ligne
-					JButton btn40 = new JButton("New button");
-					Boutons.add(btn40);
-					
-					JButton btn41 = new JButton("New button");
-					Boutons.add(btn41);
-					
-					JButton btn42 = new JButton("New button");
-					Boutons.add(btn42);
-					
-					JButton btn43 = new JButton("New button");
-					Boutons.add(btn43);
-					
-					JButton btn44 = new JButton("New button");
-					Boutons.add(btn44);
-					
-					JButton btn45 = new JButton("New button");
-					Boutons.add(btn45);
-					
-				//Sixième ligne
-					JButton btn50 = new JButton("New button");
-					Boutons.add(btn50);
-					
-					JButton btn51 = new JButton("New button");
-					Boutons.add(btn51);
-					
-					JButton btn52 = new JButton("New button");
-					Boutons.add(btn52);
-					
-					JButton btn53 = new JButton("New button");
-					Boutons.add(btn53);
-					
-					JButton btn54 = new JButton("New button");
-					Boutons.add(btn54);
-					
-					JButton btn55 = new JButton("New button");
-					Boutons.add(btn55);
-	
+				JButton btn01 = new JButton("<html><center>Eau plate <br> 1,50€ </center></html>");
+				btn01.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn01);
+				
+				JButton btn02 = new JButton("<html><center> Eau pétillante <br> 1,50€ </hmtl>");
+				btn02.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn02);
+				
+				JButton btn03 = new JButton("<html><center> Coca cola <br> 1,50€ </center></html>");
+				btn03.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn03);
+				
+				JButton btn04 = new JButton("<html><center> Coca light <br> 1,50€ </center></html>");
+				btn04.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn04);
+				
+				JButton btn05 = new JButton("<html><center> Limonade <br> 1,50€ </center></html>");
+				btn05.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn05);
+			
+			//Deuxième ligne
+				JButton btn10 = new JButton("New button");
+				setInvisible(btn10);
+				Boutons.add(btn10);
+				
+				JButton btn11 = new JButton("<html><center> Ice Tea <br> 2,00€ </center></html>");
+				btn11.setBackground(new Color(8, 96, 168));
+				Boutons.add(btn11);
+				
+				JButton btn12 = new JButton("New button");
+				setInvisible(btn12);
+				Boutons.add(btn12);
+				
+				JButton btn13 = new JButton("New button");
+				setInvisible(btn13);
+				Boutons.add(btn13);
+				
+				JButton btn14 = new JButton("New button");
+				setInvisible(btn14);
+				Boutons.add(btn14);
+				
+				JButton btn15 = new JButton("<html><center> Jus de fruit <br> 1,50€ </center></html>");
+				btn15.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn15);
+				
+			//Troisième ligne
+				JButton btn20 = new JButton("<html><center> Ticket bleu <br> 2,00€ </center></html>");
+				btn20.setBackground(new Color(8, 96, 168));
+				Boutons.add(btn20);
+				
+				JButton btn21 = new JButton("<html><center> Kriek <br> 2,00€ </center></html>");
+				btn21.setBackground(new Color(8, 96, 168));
+				Boutons.add(btn21);
+				
+				JButton btn22 = new JButton("<html><center> Blanche <br> 2,00€ </center></html>");
+				btn22.setBackground(new Color(8, 96, 168));
+				Boutons.add(btn22);
+				
+				JButton btn23 = new JButton("<html><center> Verre de vin <br> 2,00€ </center></html>");
+				btn23.setBackground(new Color(8, 96, 168));
+				Boutons.add(btn23);
+				
+				JButton btn24 = new JButton("<html><center> Pils <br> 1,50€ </center></html>");
+				btn24.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn24);
+				
+				JButton btn25 = new JButton("<html><center> Gouyasse <br> 2,50€ </center></html>");
+				btn25.setBackground(new Color(255, 255 ,0));
+				Boutons.add(btn25);
+				
+			//Quatrième ligne
+				JButton btn30 = new JButton("<html><center> Ticket orange <br> 3,00€ </center></html>");
+				btn30.setBackground(new Color(255, 131 ,0));
+				Boutons.add(btn30);
+				
+				JButton btn31 = new JButton("<html><center> Chimay bleue <br> 3,00€ </center></html>");
+				btn31.setBackground(new Color(255, 131 ,0));
+				Boutons.add(btn31);
+				
+				JButton btn32 = new JButton("New button");
+				setInvisible(btn32);
+				Boutons.add(btn32);
+				
+				JButton btn33 = new JButton("New button");
+				setInvisible(btn33);
+				Boutons.add(btn33);
+				
+				JButton btn34 = new JButton("New button");
+				setInvisible(btn34);
+				Boutons.add(btn34);
+				
+				JButton btn35 = new JButton("New button");
+				setInvisible(btn35);
+				Boutons.add(btn35);
+				
+			//Cinquième ligne
+				JButton btn40 = new JButton("<html><center> Ticket jaune <br> 2,50€ </center></html>");
+				btn40.setBackground(new Color(255, 255 ,0));
+				Boutons.add(btn40);
+				
+				JButton btn41 = new JButton("<html><center> Petit déjeuner <br> 2,50€ </center></html>");
+				btn41.setBackground(new Color(255, 255 ,0));
+				Boutons.add(btn41);
+				
+				JButton btn42 = new JButton("<html><center> Sandwich <br> 2,50€ </center></html>");
+				btn42.setBackground(new Color(255, 255 ,0));
+				Boutons.add(btn42);
+				
+				JButton btn43 = new JButton("<html><center> Hot Dog <br> 2,50€ </center></html>");
+				btn43.setBackground(new Color(255, 255 ,0));
+				Boutons.add(btn43);
+				
+				JButton btn44 = new JButton("New button");
+				setInvisible(btn44);
+				Boutons.add(btn44);
+				
+				JButton btn45 = new JButton("New button");
+				setInvisible(btn45);
+				Boutons.add(btn45);
+				
+			//Sixième ligne
+				JButton btn50 = new JButton("New button");
+				setInvisible(btn50);
+				Boutons.add(btn50);
+				
+				JButton btn51 = new JButton("<html><center> Café <br> 1,50€ </center></html>");
+				btn51.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn51);
+				
+				JButton btn52 = new JButton("<html><center> Chocolat chaud <br> 1,50€ </center></html>");
+				btn52.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn52);
+				
+				JButton btn53 = new JButton("<html><center> Couque <br> 1,50€ </center></html>");
+				btn53.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn53);
+				
+				JButton btn54 = new JButton("<html><center> Chips <br> 1,50€ </center></html>");
+				btn54.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn54);
+				
+				JButton btn55 = new JButton("<html><center> Bonbon <br> 1,50€ </center></html>");
+				btn55.setBackground(new Color(255, 0 ,0));
+				Boutons.add(btn55);
 		
 			//Panneau de résumé de ce qui a été introduit
 				JPanel Resume = new JPanel();
@@ -291,6 +326,13 @@ public class ParametrageView {
 				Resume.setBackground(new Color(150, 150, 0));
 				Resume.setBorder(transparent9px);
 				Resume.setLayout(new GridLayout(2, 1, 0, 0));
+	}
+
+	private void setInvisible(JButton btn) {
+		btn.setOpaque(false);
+		btn.setVisible(false);
+		btn.setBorderPainted(false);
+		
 	}
 
 }
--- Code/src/vente/VenteView.java
+++ Code/src/vente/VenteView.java
@@ -14,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.LineBorder;
@@ -44,6 +45,12 @@ public class VenteView {
 				}
 			}
 		});
+	}
+	
+	private void setInvisible(JButton btn) {
+		btn.setOpaque(false);
+		btn.setVisible(false);
+		btn.setBorderPainted(false);
 	}
 
 	/**
@@ -166,121 +173,157 @@ public class VenteView {
 				//Boutons.setMaximumSize(new Dimension(94, 562));
 				Boutons.setPreferredSize(new Dimension(550, 617));
 				Boutons.setSize(new Dimension(550, 617));
-				Boutons.setBackground(new Color(150, 0, 150));
+				Boutons.setBackground(new Color(61, 72, 73));
 				Boutons.setBorder(transparent9px);
 				Boutons.setLayout(new GridLayout(6, 6, 0, 0));
 				//Première ligne
-					JButton btn00 = new JButton("New button");
+					JButton btn00 = new JButton("<html><center>Ticket rouge <br> 1,50€ </center></html>");
+					btn00.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn00);
 					
-					JButton btn01 = new JButton("New button");
+					JButton btn01 = new JButton("<html><center>Eau plate <br> 1,50€ </center></html>");
+					btn01.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn01);
 					
-					JButton btn02 = new JButton("New button");
+					JButton btn02 = new JButton("<html><center> Eau pétillante <br> 1,50€ </hmtl>");
+					btn02.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn02);
 					
-					JButton btn03 = new JButton("New button");
+					JButton btn03 = new JButton("<html><center> Coca cola <br> 1,50€ </center></html>");
+					btn03.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn03);
 					
-					JButton btn04 = new JButton("New button");
+					JButton btn04 = new JButton("<html><center> Coca light <br> 1,50€ </center></html>");
+					btn04.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn04);
 					
-					JButton btn05 = new JButton("New button");
+					JButton btn05 = new JButton("<html><center> Limonade <br> 1,50€ </center></html>");
+					btn05.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn05);
 				
 				//Deuxième ligne
 					JButton btn10 = new JButton("New button");
+					setInvisible(btn10);
 					Boutons.add(btn10);
 					
-					JButton btn11 = new JButton("New button");
+					JButton btn11 = new JButton("<html><center> Ice Tea <br> 2,00€ </center></html>");
+					btn11.setBackground(new Color(8, 96, 168));
 					Boutons.add(btn11);
 					
 					JButton btn12 = new JButton("New button");
+					setInvisible(btn12);
 					Boutons.add(btn12);
 					
 					JButton btn13 = new JButton("New button");
+					setInvisible(btn13);
 					Boutons.add(btn13);
 					
 					JButton btn14 = new JButton("New button");
+					setInvisible(btn14);
 					Boutons.add(btn14);
 					
-					JButton btn15 = new JButton("New button");
+					JButton btn15 = new JButton("<html><center> Jus de fruit <br> 1,50€ </center></html>");
+					btn15.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn15);
 					
 				//Troisième ligne
-					JButton btn20 = new JButton("New button");
+					JButton btn20 = new JButton("<html><center> Ticket bleu <br> 2,00€ </center></html>");
+					btn20.setBackground(new Color(8, 96, 168));
 					Boutons.add(btn20);
 					
-					JButton btn21 = new JButton("New button");
+					JButton btn21 = new JButton("<html><center> Kriek <br> 2,00€ </center></html>");
+					btn21.setBackground(new Color(8, 96, 168));
 					Boutons.add(btn21);
 					
-					JButton btn22 = new JButton("New button");
+					JButton btn22 = new JButton("<html><center> Blanche <br> 2,00€ </center></html>");
+					btn22.setBackground(new Color(8, 96, 168));
 					Boutons.add(btn22);
 					
-					JButton btn23 = new JButton("New button");
+					JButton btn23 = new JButton("<html><center> Verre de vin <br> 2,00€ </center></html>");
+					btn23.setBackground(new Color(8, 96, 168));
 					Boutons.add(btn23);
 					
-					JButton btn24 = new JButton("New button");
+					JButton btn24 = new JButton("<html><center> Pils <br> 1,50€ </center></html>");
+					btn24.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn24);
 					
-					JButton btn25 = new JButton("New button");
+					JButton btn25 = new JButton("<html><center> Gouyasse <br> 2,50€ </center></html>");
+					btn25.setBackground(new Color(255, 255 ,0));
 					Boutons.add(btn25);
 					
 				//Quatrième ligne
-					JButton btn30 = new JButton("New button");
+					JButton btn30 = new JButton("<html><center> Ticket orange <br> 3,00€ </center></html>");
+					btn30.setBackground(new Color(255, 131 ,0));
 					Boutons.add(btn30);
 					
-					JButton btn31 = new JButton("New button");
+					JButton btn31 = new JButton("<html><center> Chimay bleue <br> 3,00€ </center></html>");
+					btn31.setBackground(new Color(255, 131 ,0));
 					Boutons.add(btn31);
 					
 					JButton btn32 = new JButton("New button");
+					setInvisible(btn32);
 					Boutons.add(btn32);
 					
 					JButton btn33 = new JButton("New button");
+					setInvisible(btn33);
 					Boutons.add(btn33);
 					
 					JButton btn34 = new JButton("New button");
+					setInvisible(btn34);
 					Boutons.add(btn34);
 					
 					JButton btn35 = new JButton("New button");
+					setInvisible(btn35);
 					Boutons.add(btn35);
 					
 				//Cinquième ligne
-					JButton btn40 = new JButton("New button");
+					JButton btn40 = new JButton("<html><center> Ticket jaune <br> 2,50€ </center></html>");
+					btn40.setBackground(new Color(255, 255 ,0));
 					Boutons.add(btn40);
 					
-					JButton btn41 = new JButton("New button");
+					JButton btn41 = new JButton("<html><center> Petit déjeuner <br> 2,50€ </center></html>");
+					btn41.setBackground(new Color(255, 255 ,0));
 					Boutons.add(btn41);
 					
-					JButton btn42 = new JButton("New button");
+					JButton btn42 = new JButton("<html><center> Sandwich <br> 2,50€ </center></html>");
+					btn42.setBackground(new Color(255, 255 ,0));
 					Boutons.add(btn42);
 					
-					JButton btn43 = new JButton("New button");
+					JButton btn43 = new JButton("<html><center> Hot Dog <br> 2,50€ </center></html>");
+					btn43.setBackground(new Color(255, 255 ,0));
 					Boutons.add(btn43);
 					
 					JButton btn44 = new JButton("New button");
+					setInvisible(btn44);
 					Boutons.add(btn44);
 					
 					JButton btn45 = new JButton("New button");
+					setInvisible(btn45);
 					Boutons.add(btn45);
 					
 				//Sixième ligne
 					JButton btn50 = new JButton("New button");
+					setInvisible(btn50);
 					Boutons.add(btn50);
 					
-					JButton btn51 = new JButton("New button");
+					JButton btn51 = new JButton("<html><center> Café <br> 1,50€ </center></html>");
+					btn51.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn51);
 					
-					JButton btn52 = new JButton("New button");
+					JButton btn52 = new JButton("<html><center> Chocolat chaud <br> 1,50€ </center></html>");
+					btn52.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn52);
 					
-					JButton btn53 = new JButton("New button");
+					JButton btn53 = new JButton("<html><center> Couque <br> 1,50€ </center></html>");
+					btn53.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn53);
 					
-					JButton btn54 = new JButton("New button");
+					JButton btn54 = new JButton("<html><center> Chips <br> 1,50€ </center></html>");
+					btn54.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn54);
 					
-					JButton btn55 = new JButton("New button");
+					JButton btn55 = new JButton("<html><center> Bonbon <br> 1,50€ </center></html>");
+					btn55.setBackground(new Color(255, 0 ,0));
 					Boutons.add(btn55);
 	
 		
